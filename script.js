05c88bd75071726fb56c42f8dfcac4425044f5f5
Niki Wells <ncwells2@illinois.edu>
2015-03-24 13:28:45 -0500

    part 1 is functional

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // var element = document.getElementById("button-container");
 // element.appendChild(but);
 var myFirebaseRef = new Firebase("https://blinding-fire-3403.firebaseio.com/");
+var connectedRef = new Firebase("https://blinding-fire-3403.firebaseio.com/.info/connected");
 var buttonBase = myFirebaseRef.child('/buttons');
 
 var n = 4;
@@ -19,30 +20,54 @@ for (var i = 0; i < 2*n; i++)
 	{
 		var but = document.createElement("button");
 		var name = "button-"+(i*n + j);
-		// var thisButton = buttonBase.child('/'+name);
+		var thisButton = buttonBase.child('/'+name);
 		// thisButton.set(
 		// 	{
-		// 		"toggle": true
+		// 		"toggle": true,
+		// 		"number": (i*n + j)
 		// 	}
 		// );
 		var funct = "change(this,'"+name+"',"+(i*n + j)+")";
 		but.setAttribute("id", name);
 		but.setAttribute("onclick", funct);
 		but.classList.add("cycle");
-		//but.classList.add("on");
+		but.classList.add("on");
 		var node = document.createTextNode(name);
 		but.appendChild(node);
-		button_bool_array.push(true);
+		// thisButton.once('value', function(data) {
+ 	// 		var isOn = data.val().toggle;
+ 	// 		console.log(isOn);
+ 	// 		button_bool_array.push(isOn);
+ 	// 		if (!isOn)
+ 	// 		{
+ 	// 			but.classList.remove("on");
+ 	// 		}
+		// });
 		row.appendChild(but);
 	}
 	var element = document.getElementById("button-container");
 	element.appendChild(row);
 };
-console.log(buttonBase);
+buttonBase.orderByKey().on("child_added", function(snapshot) {
+	var item = snapshot.val()
+	button_bool_array[item.number] = item.toggle;
+	if (!(item.toggle))
+	{
+		document.getElementById(snapshot.key()).classList.remove("on");
+	}
+});
 
 buttonBase.on("child_changed", function(snapshot) {
-  var changedPost = snapshot.val();
-  console.log("The updated post title is " + changedPost.toggle);
+	var isOn = snapshot.val().toggle;
+	var elemID = snapshot.key();
+	if (isOn)
+	{
+		document.getElementById(elemID).classList.add("on");
+	}
+	else
+	{
+		document.getElementById(elemID).classList.remove("on");
+	}
 });
 
 function change (elem, name, num) {
@@ -57,8 +82,21 @@ function change (elem, name, num) {
 	}
 	else
 	{
+		var buttonRef = buttonBase.child(name);
+		buttonRef.update({
+			"toggle": true
+		});
 		elem.classList.add("on");
 		button_bool_array[num] = true;
 	}
-	// body...
 }
+
+connectedRef.on("value", function(snap) {
+  if (snap.val() === true) {
+    document.getElementById("fire").style.display = 'block';
+    document.getElementById("fire-extinguisher").style.display = 'none';
+  } else {
+    document.getElementById("fire").style.display = 'none';
+    document.getElementById("fire-extinguisher").style.display = 'block';
+  }
+});
